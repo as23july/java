7ada5e356b6c9f0b5c426772ad97349ce8f3032a
Aditya <as23july@gmail.com>
2023-04-02 17:39:22 +0530

    new java file

---
+++ Myfirst.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Myfirst {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int aa = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = aa+b; 
+        System.out.println(sum);
+        
+        
+    }
+}
+ 
