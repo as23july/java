7528396e0b15bdb57fbc3399ede0cbbd9209482c
Aditya <as23july@gmail.com>
2023-04-16 19:09:03 +0530

    if else

--- Myfirst.java
+++ Myfirst.java
@@ -3,10 +3,21 @@ import java.util.*;
 public class Myfirst {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int aa = sc.nextInt();
+        String name = "tony";
+        int a = sc.nextInt();
         int b = sc.nextInt();
-        int sum = aa+b; 
-        System.out.println(sum);
+        // int sum = aa+b; 
+        if(a == b)
+        {
+            System.out.println("equal");
+        }
+        else if(a>b){
+                System.out.println("a greater");
+            }
+        else{
+                System.out.println("b greater");
+            }
+        
         
         
     }
