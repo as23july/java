87b87965ec0f4dbdae7429027ef6dbda751e7c55
Aditya <as23july@gmail.com>
2023-05-05 09:38:46 +0530

    b

--- Myfirst.java
+++ Myfirst.java
@@ -1,14 +1,28 @@
 import java.util.*;
 import Folder.*;
+class First{
+    public void print(){
+        System.out.println("first method called");
+    }
+}
+class Second extends First{
+    public void print(){
+        System.out.println("second method called");
+    }
+}
 
 public class Myfirst {
+    
     public static void main(String[] args) {
-        Myclass myclass = new Myclass();
+
+        First me = new First();
+        me.print();
+        // Myclass myclass = new Myclass();
         
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        myclass.print(a, b);
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // myclass.print(a, b);
         // int sum = aa+b; 
         // if(a == b)
         // {
@@ -16,7 +30,7 @@ public class Myfirst {
         // }
         // else if(a>b){
         //         System.out.println("a greater");
-        
+
         //     }
         // else{
         //         System.out.println("b greater");
