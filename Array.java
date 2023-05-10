a6b624ca27a0ea7572a67a94941db4a49ca494b3
Aditya <as23july@gmail.com>
2023-05-10 23:23:47 +0530

    d

--- Array.java
+++ Array.java
@@ -18,7 +18,7 @@ public class Array {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == x ) {
                 
-                System.out.println("found at index : " + i);
+                System.out.println("found at index is : " + i);
             }
         }
     }
