68d1c09476bd7fc26feb5544bb7334613cc26ba4
Aditya <as23july@gmail.com>
2023-06-14 17:27:48 +0530

    f

---
+++ steam/Steamexp.java
@@ -0,0 +1,23 @@
+// package steam;
+
+import java.util.List;
+
+import Folder.Person;
+
+public class Steamexp {
+
+    @Test
+    public void StreamApproch() throws IOException {
+        List <Person> pepole = MockData.getPerson();
+        // List <Person> yperson =  new ArrayList<>();
+        pepole.stream()
+        int limit = 10;
+        int counter = 0;
+        for (Person person : people) {
+            
+        }
+}
+    public static void main(String[] args) {
+        
+    }
+}
